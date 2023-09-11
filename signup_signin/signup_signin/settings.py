1dd1e4a827f98b610ec4cd84193fbe47e207a6f7
Muhamad Salman Raza <salman.raza@arbisoft.com>
2023-09-11 10:30:33 +0500

    connected postgres

--- signup_signin/signup_signin/settings.py
+++ signup_signin/signup_signin/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'signup_signin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'new2sqlite3.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_training',
+        'USER': 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
