8231b645050b9bb8290f042ec82a328373df4a3e
Muhamad Salman Raza <salman.raza@arbisoft.com>
2023-10-13 11:20:23 +0500

    added read only task id

--- todo_app/task/serializers.py
+++ todo_app/task/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    taskId = serializers.ReadOnlyField(source='task_id')
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'status']
+        fields = ['title', 'description', 'due_date', 'status', 'taskId']
